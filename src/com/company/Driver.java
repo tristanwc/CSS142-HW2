a3b3194d64057d9bfaded6c4bd704db046fed14f
Tristan Cortez <tristan.cortez007@gmail.com>
2018-10-10 23:15:01 -0700

    test

--- src/com/company/Driver.java
+++ src/com/company/Driver.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Driver {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in); //Gets input
 
         double total = computeTotal(input);
         System.out.println("The total is cost for the 4 meals is " + total);
